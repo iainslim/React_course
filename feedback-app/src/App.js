e6ec9eee6c9c87e9aefee2b387485192d6613097
Iain Slim <iain.j.slim@outlook.com>
2022-01-24 11:28:18 -0500

    Intro to Routing and creating Links

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,9 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import {useState} from 'react'
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutIconLink from './components/AboutIconLink';
+import AboutPage from './pages/AboutPage';
+
 import FeedbackData from './data/FeedbackData';
 
 function App() {
@@ -23,15 +27,25 @@ function App() {
 
     return (
         // Elements must be part of a single parent element (allows to include multiple lines)
-        <>
+        // Wrapper everything in 'Router' to use Router
+        // All 'Route' elements must be wrapped in parent 'Routes'
+        <Router>
             <Header />
             <div className="container">
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback}
-                handleDelete={deleteFeedback}/>
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback}/>
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                        </>
+                    }></Route>  
+
+                    <Route path='/about' element={<AboutPage />} />     
+                </Routes>
+                <AboutIconLink/>
             </div>
-        </>
+        </Router>
         
     )
 }
